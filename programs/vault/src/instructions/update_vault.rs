9ab3a0f191bc8aa5c82cb7d11e8a637298e999ba
LHF009 <rodriguezpatricio567@gmail.com>
2026-01-27 14:52:19 -0300

    Update update_vault.rs

--- programs/vault/src/instructions/update_vault.rs
+++ programs/vault/src/instructions/update_vault.rs
@@ -34,5 +34,19 @@ pub struct UpdateVault<'info> {
 }
 
 pub fn handler<'info>(ctx: Context<UpdateVault>, args: UpdateVaultArgs) -> Result<()> {
+    ctx.accounts.vault.set_inner(VaultConfig {
+        asset_mint_address: ctx.accounts.asset_mint.key(),
+        share_mint_address: ctx.accounts.share_mint.key(),
+        vault_token_account: ctx.accounts.reserve.key(),
+        authority: ctx.accounts.authority.key(),
+        initial_price: 0,
+        deposit_fees: args.deposit_fees.unwrap_or(ctx.accounts.vault.deposit_fees),
+        withdraw_fees: args
+            .withdraw_fees
+            .unwrap_or(ctx.accounts.vault.withdraw_fees),
+        paused: args.paused.unwrap_or(ctx.accounts.vault.paused),
+        vault_asset_cap: args.vault_asset_cap.unwrap_or(0),
+        total_asset_balance: 0,
+    });
     Ok(())
 }
