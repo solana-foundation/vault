194174500e60c461b30ae6989db8681c2d579b4a
LHF009 <rodriguezpatricio567@gmail.com>
2026-01-27 16:38:07 -0300

    Update lib.rs

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -12,10 +12,11 @@ pub mod vault {
 
     use super::*;
 
+    /// Initialize an atomic vault.
     pub fn create_vault(ctx: Context<CreateVault>, args: VaultArgs) -> Result<()> {
         instructions::create_vault::handler(ctx, args)
     }
-
+    /// Update a tokenized vault state based on the supplied arguments.
     pub fn update_vault(ctx: Context<UpdateVault>, args: UpdateVaultArgs) -> Result<()> {
         instructions::update_vault::handler(ctx, args)
     }
