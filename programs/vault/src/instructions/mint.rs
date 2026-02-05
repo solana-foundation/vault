eda9454ce3ae7af9cb9fb51f496f8a9f914749d9
LHF009 <rodriguezpatricio567@gmail.com>
2026-02-05 16:59:30 -0300

    Update mint.rs

--- programs/vault/src/instructions/mint.rs
+++ programs/vault/src/instructions/mint.rs
@@ -6,15 +6,15 @@ pub fn handler<'info>(ctx: Context<Deposit>, shares: u64) -> Result<()> {
     require!(!ctx.accounts.vault.paused, VaultProgramError::PausedVault);
 
     let assets = ctx.accounts.vault.get_assets_from_shares(
-        &ctx.accounts.share_mint,
+        ctx.accounts.share_mint.supply,
         shares,
         Rounding::Up,
     )?;
     let fee = ctx.accounts.vault.get_deposit_fee_when_minting(assets)?;
     ctx.accounts.vault.increase_asset_supply(assets)?;
-    ctx.accounts.transfer_reserve_token_to_vault(assets)?;
+    ctx.accounts.transfer_asset_token_to_vault(assets)?;
     ctx.accounts
-        .transfer_reserve_token_fee_to_fee_recipient(fee)?;
-    ctx.accounts.mint(shares)?;
+        .transfer_asset_token_fee_to_fee_recipient(fee)?;
+    ctx.accounts.mint_shares_to_user(shares)?;
     Ok(())
 }
