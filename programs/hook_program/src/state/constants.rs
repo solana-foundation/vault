737096b97d05e0690304379ced5536bcc2195618
LHF009 <rodriguezpatricio567@gmail.com>
2026-03-27 16:24:17 -0300

    removed discriminators

--- programs/hook_program/src/state/constants.rs
+++ programs/hook_program/src/state/constants.rs
@@ -7,6 +7,6 @@ pub const VAULT_ASSOCIATED_PROTOCOLS_SEED: &[u8] = b"vault_associated_protocols"
 pub const VAULT_PROTOCOL_DEPOSIT_SEED: &[u8] = b"vault_protocol_deposit";
 pub const VAULT_SEED: &[u8] = b"vault";
 pub const VAULT_NAV_DATA_SEED: &[u8] = b"vault_nav_data";
-pub const UPDATE_NAV_DISCRIMINATOR: [u8; 8] = [56, 16, 234, 109, 155, 165, 5, 0];
+
 pub const VAULT_PROGRAM_ID: &anchor_lang::prelude::Pubkey =
     &pubkey!("ANXYYTDoEHooFjaN8M8pDHRj87d945Bj5QvAFGcpqakw");
